b96c7a5e3aa366830ae1b4f4eba9a030fd22e13b
xhs <1170495472@qq.com>
2022-04-09 19:04:12 +0800

    springcloud父工程环境搭建，包含基本pom

---
+++ cloud_provider_payment8001/src/main/java/com/xxx/springcloud/PaymentMain8001.java
@@ -0,0 +1,18 @@
+package com.xxx.springcloud;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * description: springcloud 单个微服务系统搭建
+ * author: xhs 
+ * date: 2022/4/3
+ * version: 
+ **/
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+}
